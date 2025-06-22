2bef41c3c9b925be7122f5ebe32fca4704d11ca1
Vadym Tilihuzov <71652431+Real-4339@users.noreply.github.com>
2025-06-22 18:53:13 +0200

    Update sound.nix

--- nixos/modules/sound.nix
+++ nixos/modules/sound.nix
@@ -1,12 +1,14 @@
 {
-  hardware.pulseaudio.enable = false;
-  sound.enable = true;
+  # hardware.pulseaudio.enable = false; # Deleted in 25.05
+  services.pulseaudio.enable = false;
+  # sound.enable = true; # Was deleted in 24.11
 
   # rtkit is optional but recommended
   security.rtkit.enable = true;
 
   services.pipewire = {
     enable = true;
+    audio.enable = true;
 
     alsa.enable = true;
     alsa.support32Bit = true;
@@ -15,9 +17,9 @@
     jack.enable = true;
     
     # Bluetooth Configuration
-    bluetooth = {
-      enable = true;  # Enables PipeWire's Bluetooth audio support
-      a2dp.defaultCodec = "aptx";  # Or "ldac", if needed
-    };
+    #bluetooth = {
+    #  enable = true;  # Enables PipeWire's Bluetooth audio support
+    #  a2dp.defaultCodec = "aptx";  # Or "ldac", if needed
+    #};
   };
 }
