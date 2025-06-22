d76a6925e8ca9fe7e4d4e7a0cb4bcd380a431d64
Vadym Tilihuzov <71652431+Real-4339@users.noreply.github.com>
2025-06-22 18:55:06 +0200

    Update xserver.nix

--- nixos/modules/xserver.nix
+++ nixos/modules/xserver.nix
@@ -1,26 +1,24 @@
 {
   services.xserver = {
     enable = true;
-    windowManager.herbstluftwm.enable = true;
+    windowManager.awesome.enable = true;
+    # windowManager.herbstluftwm.enable = true;
 
-    displayManager = {
-      autoLogin.enable = true;
-      autoLogin.user = "unreal";
-      lightdm.enable = true;
+    displayManager.lightdm = {
+      greeters.gtk.enable = true;
+      enable = true;
     };
 
-    layout = "us";
-    xkbVariant = "";
+    xkb.layout = "us, ru";
+    xkb.variant = "";
+    xkbOptions = "grp:alt_shift_toggle";
 
-    libinput = {
+    # videoDrivers = [ "nvidia" ];
+    # deviceSection = ''Option "TearFree" "True"'';
+  };
+  services.libinput = {
       enable = true;
       mouse.accelProfile = "flat";
       touchpad.accelProfile = "flat";
-    };
-
-    videoDrivers = [ "nvidia" ];
-    # deviceSection = ''Option "TearFree" "True"'';
-    #displayManager.gdm.enable = true;
-    #desktopManager.gnome.enable = true;
   };
 }
